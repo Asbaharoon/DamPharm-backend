8b8f2d7726411931bb08d9201dca96987b7c64d3
salahatwa <salahsayedatwa@gmail.com>
2021-07-14 16:32:51 +0200

    fix id issue

--- src/main/java/run/dampharm/app/domain/Invoice.java
+++ src/main/java/run/dampharm/app/domain/Invoice.java
@@ -30,8 +30,8 @@ public class Invoice extends UserDateAudit implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "book_seq")
-	@GenericGenerator(name = "book_seq", strategy = "run.dampharm.app.utils.InvoiceCodePrefixedSequenceIdGenerator", parameters = {
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "inv_seq")
+	@GenericGenerator(name = "inv_seq", strategy = "run.dampharm.app.utils.InvoiceCodePrefixedSequenceIdGenerator", parameters = {
 			@Parameter(name = InvoiceCodePrefixedSequenceIdGenerator.INCREMENT_PARAM, value = "50"),
 			@Parameter(name = InvoiceCodePrefixedSequenceIdGenerator.VALUE_PREFIX_PARAMETER, value = "INV_"),
 			@Parameter(name = InvoiceCodePrefixedSequenceIdGenerator.NUMBER_FORMAT_PARAMETER, value = "%05d") })
@@ -56,10 +56,7 @@ public class Invoice extends UserDateAudit implements Serializable {
 		Double total = 0.0;
 		int size = items.size();
 
-		System.err.println(size);
-
 		for (int i = 0; i < size; i++) {
-			System.err.println(items.get(i).getQuantity());
 			total += items.get(i).calculateImport();
 		}
 
